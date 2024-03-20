1bb53750b06b44fc4653c084c44bd633804db71c
lalotone <lalotone@gmail.com>
2024-03-20 22:04:13 +0100

    Add K6 test

---
+++ tests/k6/sample-test.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '100s', target: 10 },  // Start with 10 clients for 100 seconds
+        { duration: '20s', target: 20 },   // Then increase to 20 clients for 20 seconds
+        { duration: '100s', target: 200 }  // Finally, increase to 200 clients for 100 seconds
+    ],
+    thresholds: {
+        http_req_duration: ['p(95)<500'], // Thresholds for response time
+    }
+};
+
+export default function () {
+    // Send GET request to the Kubernetes service
+    let res = http.get('http://testkube-demo-blue-service.default');
+    // Log the response status and duration
+    console.log(`Status: ${res.status}, Response time: ${res.timings.duration} ms`);
+    // Sleep for a short duration between requests
+    sleep(1);
+}
